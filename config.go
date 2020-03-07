d59f8b979ff19ae2ea973a3f916637dafad3f3d3
miaojia <mj37yhyy@yeah.net>
2020-03-07 09:10:41 +0800

    支持consul多集群、单集群多节点配置

--- config.go
+++ config.go
@@ -22,17 +22,17 @@ type Application struct {
 	Config Config `mapstructure:"config"`
 }
 
+type Config struct {
+	Remote []Remote `mapstructure:"remote"`
+	File   bool     `mapstructure:"file"`
+}
+
 type Remote struct {
 	Enabled       bool     `mapstructure:"enabled"`
 	Provider      string   `mapstructure:"provider"`
-	Endpoint      string   `mapstructure:"endpoint"`
+	Endpoint      []string `mapstructure:"endpoint"`
 	Path          []string `mapstructure:"path"`
 	SecretKeyring string   `mapstructure:"secret-keyring"`
-}
-
-type Config struct {
-	Remote Remote `mapstructure:"remote"`
-	File   bool   `mapstructure:"file"`
 }
 
 func InitConfig(obj interface{}) error {
@@ -45,46 +45,65 @@ func InitConfig(obj interface{}) error {
 			return err
 		}
 	}
-	if root.Application.Config.Remote.Enabled {
-		if root.Application.Config.Remote.SecretKeyring != "" {
-			for _, path := range root.Application.Config.Remote.Path {
-				if err := NewRemoteConfig(
-					func(runtimeViper *viper.Viper) error {
-						if err := runtimeViper.AddSecureRemoteProvider(
-							root.Application.Config.Remote.Provider,
-							root.Application.Config.Remote.Endpoint,
-							path,
-							root.Application.Config.Remote.SecretKeyring,
-						); err != nil {
-							return err
-						}
-
-						return nil
-					},
-					&obj,
-				); err != nil {
-					return err
-				}
-			}
-		} else {
-			for _, path := range root.Application.Config.Remote.Path {
-				if err := NewRemoteConfig(
-					func(runtimeViper *viper.Viper) error {
-						if err := runtimeViper.AddRemoteProvider(
-							root.Application.Config.Remote.Provider,
-							root.Application.Config.Remote.Endpoint,
-							path,
-						); err != nil {
-							return err
-						}
-						return nil
-					},
-					&obj,
-				); err != nil {
-					return err
+	for _, remote := range root.Application.Config.Remote {
+		if remote.Enabled {
+			for _, path := range remote.Path {
+				if remote.SecretKeyring != "" {
+					err := AddSecureRemoteProvider(remote, path, obj)
+					if err != nil {
+						return err
+					}
+				} else {
+					err := AddRemoteProvider(remote, path, obj)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
+	}
+	return nil
+}
+
+func AddRemoteProvider(remote Remote, path string, obj interface{}) error {
+	if err := NewRemoteConfig(
+		func(runtimeViper *viper.Viper) error {
+			for _, endpoint := range remote.Endpoint {
+				if err := runtimeViper.AddRemoteProvider(
+					remote.Provider,
+					endpoint,
+					path,
+				); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+		&obj,
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+func AddSecureRemoteProvider(remote Remote, path string, obj interface{}) error {
+	if err := NewRemoteConfig(
+		func(runtimeViper *viper.Viper) error {
+			for _, endpoint := range remote.Endpoint {
+				if err := runtimeViper.AddSecureRemoteProvider(
+					remote.Provider,
+					endpoint,
+					path,
+					remote.SecretKeyring,
+				); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+		&obj,
+	); err != nil {
+		return err
 	}
 	return nil
 }
